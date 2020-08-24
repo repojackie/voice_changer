4eb2d7a0841c8d822ef170b1ee34897cd1cbb40d
Jackie <repojackie@gmail.com>
2020-08-24 18:23:52 -0400

    need a framework for doing the modules

---
+++ app/Effect.java
@@ -0,0 +1,20 @@
+public class Effect
+{
+	private String name;
+	private String filename;
+
+	public Effect(String name)
+	{
+		this.name = name;
+	}
+
+	public String getName()
+	{
+		return this.name;
+	}
+
+	public void setFile(String filename)
+	{
+		this.filename = filename;
+	}
+}
---
+++ app/Effect_One.java
@@ -0,0 +1,7 @@
+public class Effect_One extends Effect
+{
+	public Effect_One(String name)
+	{
+		super(name);
+	}
+}
